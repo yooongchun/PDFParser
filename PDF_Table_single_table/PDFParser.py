e40bafc0dc1ad5bb13aa01164229dc2dfbc2aa69
zha yongchun <email@1729465178@qq.com>
2018-09-23 16:30:47 +0800

    version-2:use-line to search....

--- PDF_Table_single_table/PDFParser.py
+++ PDF_Table_single_table/PDFParser.py
@@ -9,62 +9,66 @@ import multiprocessing
 from datetime import datetime
 
 
-def search_tables(path):
+def search_page(path):
     print('load file:', os.path.basename(path))
     pdf = pdfplumber.open(path)
     pages = pdf.pages
     print('total page:', len(pages))
     print('searching pdf...')
     target = []
+    st_flag = False
     for ind, page in enumerate(pages):
         # print('parse page:', ind + 1)
         text = page.extract_text()
         lines = re.split(r'\n+', text)
         for index, line in enumerate(lines):
-            if re.match(r'\s*\d+[、.\s]+税金及附加\s*$', line):
-                tables = page.extract_tables()
-                for table in tables:
-                    row = re.sub(r'\s+', '', ''.join([item if item is not None else '' for item in table[0]]))
-                    next = lines[index + 1]
-                    cnt = index + 1
-                    while cnt < len(lines) and re.search(r'适用|不适用|单位.*元', next):
-                        cnt += 1
-                        next = lines[cnt]
-                    if re.sub(r'\s+', '', next) == row:
-                        target.append(table)
-                        if '合计' not in str(table):
-                            tables = pages[ind + 1].extract_tables()
-                            if isinstance(tables, list) and len(tables) > 0:
-                                target.append(tables[0])
-                                return target
-    return target
+            if not st_flag and re.match(r'\s*\d+[、.\s]+税金及附加\s*$', line):
+                st_flag = True
+                continue
+            if st_flag and '合计' not in line:
+                target.append(line)
+            elif st_flag and '合计' in line:
+                return target
 
 
-def target(tables, rules):
+def target(lines, rules):
     target_info = []
-    for table in tables:
-        for row in table:
-            for rule in rules:
-                if row[0] is not None and re.sub(r'\s+|\n+', '', row[0]) == rule:
-                    target_info.append(row)
+    for line in lines:
+        items = line.split(' ')
+        if not isinstance(items, list) or len(items) < 2:
+            continue
+        for rule in rules:
+            if rule == items[0]:
+                target_info.append(items)
     return target_info
 
 
-def save(info, out_path, rules, code, name):
+def saver(out_path, rules):
     print('save to file:', os.path.basename(out_path))
+    files = os.listdir('./')
+
     book = xlrd.open_workbook(out_path)
     cbook = copy(book)
     sheet = book.sheet_by_index(0)
     csheet = cbook.get_sheet('Sheet1')
 
-    csheet.write(sheet.nrows, 0, str(code))
-    csheet.write(sheet.nrows, 1, str(name))
-    for ind, one in enumerate(info):
-        if not isinstance(one, list) or len(one) < 2:
-            continue
-        for index, rule in enumerate(rules):
-            if one[0] == rule:
-                csheet.write(sheet.nrows, index + 2, str(one[1]))
+    for index, file in enumerate(files):
+        info = []
+        if os.path.isfile(file) and os.path.splitext(file)[1] == '.tmp':
+            code = re.split('-', file)[1]
+            name = re.split(r'[\-:：]', file)[2]
+            with open(file, 'r', encoding='utf-8') as fp:
+                lines = fp.readlines()
+            for line in lines:
+                info.append(line.split('|'))
+            csheet.write(sheet.nrows + index, 0, str(code))
+            csheet.write(sheet.nrows + index, 1, str(name))
+            for ind, one in enumerate(info):
+                if len(one) < 2:
+                    continue
+                for ind2, rule in enumerate(rules):
+                    if one[0] == rule:
+                        csheet.write(sheet.nrows + index, ind2 + 2, str(one[1]))
     cbook.save(out_path)
 
 
@@ -76,7 +80,7 @@ def load_demo(path):
     return row
 
 
-def folder(folder_path):
+def load_folder(folder_path):
     paths = []
     for dirpath, dirnames, filenames in os.walk(folder_path):
         for file in filenames:
@@ -86,68 +90,60 @@ def folder(folder_path):
     return paths
 
 
-def run(path, row, lock):
+def run(path, row):
     # print('processing on file:', os.path.basename(path))
     pdf = os.path.basename(path)
     rule = row[2:]
-    code = pdf.split('-')[1]
-    name = re.split(r'[\-:：]+', pdf)[-2]
     try:
-        tables = search_tables(path)
+        lines = search_page(path)
     except Exception as e:
         print('parse pdf error:', e)
         with open('errorList.txt', 'a', encoding='utf-8') as fp:
             fp.write('parse pdf error:' + path + '\n')
         return
     try:
-        info = target(tables, rule)
+        info = target(lines, rule)
         if not isinstance(info, list) or len(info) < 1:
             print('find nothing in file:', pdf)
             with open('errorList.txt', 'a', encoding='utf-8') as fp:
                 fp.write('find nothing in file:' + path + '\n')
             return
     except Exception as e:
-        print('search table error:', e)
+        print('search lines error:', e)
         with open('errorList.txt', 'a', encoding='utf-8') as fp:
-            fp.write('search table error:' + path + '\n')
+            fp.write('search lines error:' + path + '\n')
         return
     try:
-        lock.acquire()
-        save(info, demo, rule, code, name)
-        lock.release()
+        with open(pdf + '.tmp', 'w', encoding='utf-8') as fp:
+            for one in info:
+                for item in one:
+                    fp.write(str(item) + '|')
+                fp.write('\n')
     except Exception as e:
-        print('Do you open the Demo Excel? Please close it.The program will save again after 30 seconds...')
-        time.sleep(30)
-        try:
-            lock.acquire()
-            save(info, demo, rule, code, name)
-            lock.release()
-        except Exception as e:
-            print('save error:', e)
-            with open('errorList.txt', 'a', encoding='utf-8') as fp:
-                fp.write('save error:' + path + '\n')
+        print('save tmp file error:', e)
 
 
 def multi_threads(paths, row):
     th_pool = []
-    lock = threading.Lock()
     for i, path in enumerate(paths):
-        th = threading.Thread(target=run, args=(path, row, lock))
+        th = threading.Thread(target=run, args=(path, row))
         th.start()
         th_pool.append(th)
     for th in th_pool:
         th.join()
 
 
-def batch_parser(paths, demo):
+def batch_parser(folder, demo):
     try:
+        paths = load_folder(folder)
         row = load_demo(demo)
     except Exception as e:
-        print('load Excel error:', e)
+        print('load Excel/folder error:', e)
         return
+    print('total {} files.'.format(len(paths)))
     pool = multiprocessing.Pool(processes=4)
     for i in range(0, len(paths), 5):
-        path = paths[i:(i + 5 if (i + 5 <= len(paths) - 1) else len(paths) - 1)]
+        path = paths[i:(i + 5 if (i + 5 <= len(paths) - 1) else len(paths))]
         pool.apply_async(func=multi_threads, args=(path, row))
     pool.close()
     pool.join()
@@ -159,9 +155,6 @@ if __name__ == '__main__':
     demo = r'C:\Users\fanyu\Desktop\PaidProject\16_Tables_PDF_extractor\Demo.xls'
     with open('errorList.txt', 'w', encoding='utf-8')as fp:
         fp.write(str(datetime.now()) + '\n\n')
-    try:
-        paths = folder(base_dir)
-        batch_parser(paths, demo)
-    except Exception as e:
-        print('load pdf error:', e)
+    # batch_parser(base_dir, demo)
+    saver(demo, load_demo(demo)[2:])
     print('Program finished!')
